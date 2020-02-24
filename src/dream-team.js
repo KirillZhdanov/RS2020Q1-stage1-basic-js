99a6a6ec148db0a5cb45ef8d662d12be49067bdc
KirillZh <kirill.zhdanov97@gmail.com>
2020-02-24 17:39:44 +0300

    feat: Need fix for 2 test cases

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,18 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+ let team,acronym="";
+ let arr=members.flat();
+ console.log(members);
+ 
+ if(Array.isArray(members)){
+ for(const element of arr){
+  if(typeof(element)===typeof("")){
+  acronym+=element.replace(/\s/g,"").substr(0,1).toUpperCase();}
+   
+}
+
+  team=acronym.split('').sort().join('');
+console.log(team);
+return team;
+ }
+ return false;
 };
