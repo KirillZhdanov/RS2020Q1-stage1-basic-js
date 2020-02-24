0ed910616660c1cb575f84fe45619c45c9d3aa9c
KirillZhdanov <kirill.zhdanov97@gmail.com>
2020-02-24 20:16:36 +0300

    feat: Solution added

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,17 +1,12 @@
 module.exports = function createDreamTeam(members) {
  let team,acronym="";
- let arr=members.flat();
- console.log(members);
  
  if(Array.isArray(members)){
- for(const element of arr){
-  if(typeof(element)===typeof("")){
-  acronym+=element.replace(/\s/g,"").substr(0,1).toUpperCase();}
-   
+ for(const element of members){
+  if(typeof(element)===typeof(""))
+  acronym+=element.replace(/\s/g,"").substr(0,1).toUpperCase();
 }
-
   team=acronym.split('').sort().join('');
-console.log(team);
 return team;
  }
  return false;
