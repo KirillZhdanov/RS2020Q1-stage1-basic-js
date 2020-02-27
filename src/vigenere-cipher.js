4b4eeb381b9010a9f62b4187099c2d2d966307c2
KirillZh <jd_senar@mail.ru>
2020-02-27 08:08:00 +0300

    Add files via upload

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,10 +1,33 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(message,key) {
+       // const alphabet=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+       console.log(message);
+        message=message.toUpperCase().split("");
+        console.log(message);
+        let messageCodes=[];
+        let keys=key.toUpperCase().split("");
+        var i=0;
+        /*for(let j=0;j<message.length;j++){
+            messageCodes.push((message[j].charCodeAt()));
+            i++;
+            if(i>key.length)
+            i=0;
+        }*/
+        for(const element of message){
+            messageCodes.push((element.toString().charCodeAt()+keys[i].toString().charCodeAt())%26)
+            i++;
+            if(i>keys.length)
+            i=0;
+        }
+        console.log(messageCodes);
+        let result=[];
+        for(const element of messageCodes){
+            result.push(String.fromCharCode(element));
+        }
+        return result.join("");
     }
 
-    decrypt() {
+    decrypt(message,key) {
         throw 'Not implemented';
         // remove line with error and write your code here
     }
