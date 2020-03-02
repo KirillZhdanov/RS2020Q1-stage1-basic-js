75dda3108e6fecc6a4b24a22172fc9d3d5406a70
KirillZh <kirill.zhdanov97@gmail.com>
2020-03-02 17:31:43 +0300

    feat:practically works

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,28 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    let res=[],index=[],flag=false;
+    if(arr instanceof Array){
+        arr.forEach((element,i)=>{
+        if(element=="--discard-next"){
+          flag=true;
+        }
+        else 
+        if(element=="--discard-prev"){
+            res=res.splice(res.length,1);
+            res.pop();
+        }
+        else 
+        if(element=="--double-next"){
+            if(i!=arr.length)
+            res.push(arr[i+1]);
+        }
+        else 
+        if(element=="--double-prev"){
+                if(i>0)res.push(arr[i-1]);}
+        else 
+        res.push(element);
+    })
+    
+    return res;}
+    else 
+    throw Error();
 };
